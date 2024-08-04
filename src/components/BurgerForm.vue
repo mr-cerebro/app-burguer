78e72589ac3bd5426ad871a35f91d331446010c3
Paulo Pessoa <mrcerebro8@gmail.com>
2024-08-04 02:24:16 +0100

    Add form

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <p>Componente de Mensagem</p>
+  </div>
+  <div>
+    <form id="burger-form">
+      <div class="input-container">
+        <label for="nome">Nome do cliente:</label>
+        <input
+          type="text"
+          name="nome"
+          id="nome"
+          v-model="nome"
+          placeholder="Digite o seu nome..."
+        />
+      </div>
+      <div class="input-container">
+        <label for="pao">Escolha o pão:</label>
+        <select name="pao" id="pao" v-model="pao">
+          <option value="">Selecione o seu pão</option>
+          <option value="integral">Integral</option>
+        </select>
+      </div>
+      <div class="input-container">
+        <label for="carne">Escolha a carne do seu Burger</label>
+        <select name="carne" id="carne" v-model="carne">
+          <option value="">Selecione o tipo de carne</option>
+          <option value="maminha">Maminha</option>
+        </select>
+      </div>
+      <div class="input-container" id="opcionais-container">
+        <label for="opcionais" id="opcionais-title"
+          >Selecione os opcionais:</label
+        >
+        <div class="checkbox-container">
+          <input
+            type="checkbox"
+            name="opcionais"
+            v-model="opcionais"
+            value="salame"
+          />
+          <span>Salame</span>
+        </div>
+        <div class="checkbox-container">
+          <input
+            type="checkbox"
+            name="opcionais"
+            v-model="opcionais"
+            value="salame"
+          />
+          <span>Salame</span>
+        </div>
+        <div class="checkbox-container">
+          <input
+            type="checkbox"
+            name="opcionais"
+            v-model="opcionais"
+            value="salame"
+          />
+          <span>Salame</span>
+        </div>
+        <div class="input-container">
+          <input type="submit" class="submit-btn" value="Criar meu Burger" />
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BurgerForm",
+};
+</script>
+
+<style scoped>
+#burger-form {
+  max-width: 400px;
+  margin: 0 auto;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+  color: #222;
+  margin-bottom: 15px;
+  padding: 5px 10px;
+  border-left: 4px solid #fcba03;
+}
+
+input,
+select {
+  padding: 5px 10px;
+  width: 300px;
+}
+
+#opcionais-container {
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+#opcionais-title {
+  width: 100%;
+}
+
+.checkbox-container {
+  display: flex;
+  align-items: flex-start;
+  width: 50%;
+  margin-bottom: 20px;
+}
+
+.checkbox-container span,
+.checkbox-container input {
+  width: auto;
+}
+
+.checkbox-container span {
+  margin-left: 6px;
+  font-weight: bold;
+}
+
+.submit-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  font-size: 16px;
+  padding: 10px;
+  margin: 0 auto;
+  border: 2px solid #222;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.submit-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,18 @@
   <div class="main-container">
     <h1>Monte o seu Burger</h1>
   </div>
+  <BurgerForm />
 </template>
 
 <script>
 import Banner from "@/components/Banner.vue";
+import BurgerForm from "@/components/BurgerForm.vue";
 
 export default {
   name: "Home",
   components: {
     Banner,
+    BurgerForm,
   },
 };
 </script>
